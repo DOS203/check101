55dafc3ef79eba4d9a75fbce45134eb255ce921b
dos203 <dinoshanr@gmail.com>
2019-08-28 21:50:21 +1000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const UserSchema = new Schema({
+const UserSchema = mongoose.Schema({
   firstname:{
     type: String,
     required: true
@@ -24,6 +24,7 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now
   }
+ 
   // privilege:{
   //   type: String,
   //   required: false
@@ -34,4 +35,4 @@ const UserSchema = new Schema({
   // },
 });
 
-mongoose.model('users', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
