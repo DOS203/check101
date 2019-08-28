61b66219ed254da8705ffa4584d73ebb637c03fb
dos203 <dinoshanr@gmail.com>
2019-08-29 09:41:52 +1000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,15 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = mongoose.Schema({
-  name:{
+  firstname:{
     type: String,
     required: true
   },
+  lastname:{
+    type: String,
+    required: true
+  },
+  
   email:{
     type: String,
     required: true
